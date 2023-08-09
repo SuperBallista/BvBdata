eab5ef31490e229f098b69943b964ab41222df90
SuperBallista <140693798+SuperBallista@users.noreply.github.com>
2023-08-09 17:09:33 +0900

    test comit

---
+++ web.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use(express.static('public'));
+
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+
+var app = http.createServer(function(request, response) {
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    var pathname = url.parse(_url, true).pathname;
+
+    if (pathname === '/') {
+        fs.readFile('main.html', 'utf8', function(err, content) {
+            if (err) {
+                response.writeHead(500); // Internal Server Error
+                response.end('Internal Server Error');
+            } else {
+                response.writeHead(200, { 'Content-Type': 'text/html' });
+                response.end(content);
+            }
+        });
+    }});
+
+app.listen(3000);
