fbe1478d5fbde1bd9ffd4d3b3ba732bb3ce092e9
SuperBallista <kor8240@gmail.com>
2023-08-09 18:22:46 +0900

    te

--- web.js
+++ web.js
@@ -1,29 +1,15 @@
-var express = require('express');
-var app = express();
-var path = require('path');
-
-app.use(express.static('public'));
-
 var http = require('http');
 var fs = require('fs');
-var url = require('url');
-
-
-var app = http.createServer(function(request, response) {
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    var pathname = url.parse(_url, true).pathname;
-
-    if (pathname === '/') {
-        fs.readFile('index.html', 'utf8', function(err, content) {
-            if (err) {
-                response.writeHead(500); // Internal Server Error
-                response.end('Internal Server Error');
-            } else {
-                response.writeHead(200, { 'Content-Type': 'text/html' });
-                response.end(content);
-            }
-        });
-    }});
-
+var app = http.createServer(function(request,response){
+    var url = request.url;
+    if(request.url == '/'){
+      url = '/index.html';
+    }
+    if(request.url == '/favicon.ico'){
+      return response.writeHead(404);
+    }
+    response.writeHead(200);
+    response.end(fs.readFileSync(__dirname + url));
+ 
+});
 app.listen(3000);
